2cd8a75591d682a636158e8c543c0b5f8f9d8180
Vicky-1502 <79531362+Vicky-1502@users.noreply.github.com>
2023-01-24 12:48:13 +0530

    Add files via upload

---
+++ koko_eating_banana.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int Solve(int N, vector<int>& piles, int H) {
+        // Code here
+        int low = 1, high = *max_element(piles.begin(), piles.end());
+        int ans=0;
+        
+        while(low<=high){
+            int mid = (low+high)/2;
+            int tme = timefnc(mid, piles);
+            
+            if(tme <= H){
+                ans = mid;
+                high = mid-1;
+            }
+            
+            else 
+                low = mid+1;
+        }
+        return ans;
+        
+    }
+        int timefnc(int mid, vector<int>& piles) {
+            int sum=0;
+            for(int i=0; i< piles.size(); i++)
+                sum += (piles[i]/mid) + ((piles[i]%mid)!=0);
+                
+            return sum;
+        }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a;
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            a.push_back(x);
+        }
+        int h;
+        cin >> h;
+        Solution obj;
+
+        cout << obj.Solve(n, a, h);
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
